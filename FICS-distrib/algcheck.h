ed64a0a43dcf2adf0fe21cfca9311b973e406812
Robert Hinson <oppiet35@yahoo.com>
2023-07-06 00:18:44 -0400

    Create algcheck.h

---
+++ FICS-distrib/algcheck.h
@@ -0,0 +1,32 @@
+/* algcheck.h
+ *
+ */
+
+/*
+    fics - An internet chess server.
+    Copyright (C) 1993  Richard V. Nash
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+/* Revision history:
+   name         email           yy/mm/dd        Change
+   Richard Nash nash@visus.com  93/10/22        Created
+*/
+
+#ifndef _ALGCHECK_H
+#define _ALGCHECK_H
+
+extern int alg_is_move();
+extern int alg_parse_move();
+extern char *alg_unparse();
+
+#endif /* _ALGCHECK_H */
